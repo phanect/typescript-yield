05c94935c5cb03aafd61c03fddd3adb0d242ebb5
TobiaszCudnik <tobiasz.cudnik@gmail.com>
2014-01-31 00:06:24 +0100

    examples refactorings yield support

--- d.ts/suspend.d.ts
+++ d.ts/suspend.d.ts
@@ -1,18 +1,25 @@
-// TODO Module
 declare function yield<T>(result: T): T;
 
 declare module suspend {
-	export interface IResume {
-		(err: any, result: any);
+	export interface IResume<T> {
+		(err: any, result: T);
+		(err: any);
+		();
 	}
 	
 	export interface IResumeRaw {
 		(result: any);
+		();
 	}
 	
-	export function resume(): IResume;
+	export function resume<T>(): IResume<T>;
+	export function fork<T>(): IResume<T>;
+	export function run(fn: Function);
+	// TODO type this somehow
+	export function join(): any[]
 	export function resumeRaw(): IResumeRaw;
 	export function async<F>(fn: F): F;
+	export function fn<F>(fn: F): F;
 }
 
 // CommonJS version
